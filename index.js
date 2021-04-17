14d6da13b2c53364149aaf6094bbf1c9d08d9d98
falvarezcolq <falvarezcolq@gmail.com>
2021-04-17 15:23:09 -0400

    4 ejercicios , 2 sin resolver

--- index.js
+++ index.js
@@ -38,9 +38,34 @@ function esPar(num){
     return num % 2 == 0
 }
 
+
+// EJERCICIO 1
 // realizar una funcion  donde se 
 // ingrese dos puntos y hallar la distancia entre ellos
 
 function distanciaP1P2(x1,y1,x2,y2){
+    distancia = Math.sqrt(((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)));
     return distancia
 }
+
+
+// EJERCICIO 2
+// Ingrese 3 numeros a,b,c
+// e imprima el mayor
+
+function returnMax(a,b,c){
+   
+   if(a<b){
+        a=b
+   }
+   if(a<c){
+       a=c
+   }
+   return a
+}
+
+
+// EJERCICIO 3 Con el radio de un circulo hallar su area
+
+// EJERCICIO 4 Determinar si un numero es primo
+
